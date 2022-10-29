671c6d090da9d444aa1f3a398b9833c94aa4ffd9
bhawi <bhawinjasperofficial@gmail.com>
2022-10-29 17:35:05 +0530

    PRACTICE

--- PRACTICE/main.py
+++ PRACTICE/main.py
@@ -1,8 +1,11 @@
 import ibm_boto3
 from ibm_botocore.client import Config, ClientError
+import PIL.Image as Image
+import io
 
-COS_ENDPOINT="https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
-COS_API_KEY_ID="cVWZ4RVLsIGhnc7FwrQxiF_AQ4PpCrTjnU5BCrJyAmQm"
+
+COS_ENDPOINT="https://s3.tok.ap.cloud-object-storage.appdomain.cloud"
+COS_API_KEY_ID="dRpfBDLhp5Y2FqwqaZHEq6cWeinyufVjZLRz0VNl7Hnj"
 COS_INSTANCE_CRN="crn:v1:bluemix:public:cloud-object-storage:global:a/702af44240f54d66ba7adebefb61dd74:21d01580-e4e2-41a3-8589-ef29aaacb70d::"
 COS_BUCKET_LOCATION="jp-tok-smart"
 
@@ -24,4 +27,61 @@ def get_buckets():
     except Exception as e:
         print("Unable to retrieve list buckets: {0}".format(e))
 
-get_buckets()
+
+def create_bucket(bucket_name):
+    print("Creating new bucket: {0}".format(bucket_name))
+    try:
+        cos.Bucket(bucket_name).create()
+        print("Bucket: {0} created!".format(bucket_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to create bucket: {0}".format(e))
+
+def multi_part_upload(bucket_name, item_name, file_path):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 5
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 15
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+
+        with open(file_path, "rb") as file_data:
+             cos.Object(bucket_name, item_name).upload_fileobj(
+                Fileobj=file_data,
+                Config=transfer_config
+            )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+
+
+def get_item(bucket_name, item_name):
+    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
+    try:
+        file = cos.Object(bucket_name, item_name).get()
+
+        by=file["Body"].read()
+        img=Image.open(io.BytesIO(by))
+        img.save("madeit.png")
+
+
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve file contents: {0}".format(e))
+
+
--- PRACTICE/sendgridconnection.py
+++ PRACTICE/sendgridconnection.py
@@ -1,3 +1,11 @@
 import sendgrid
 from sendgrid.helpers.mail import *
+import json
+import urllib.request
+
+file=urllib.request.urlopen("https://jasper.s3.jp-tok.cloud-object-storage.appdomain.cloud/sample.json")
+
+data=json.load(file)
+print(data["bhawin"])
+
 
